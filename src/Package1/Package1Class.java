67c96471d9d10704a129a1160c9eec1e5bd20edb
helen <helen@LAPTOP-QN3F12HT.mynetworksettings.com>
2023-01-12 19:21:52 -0500

    second print statment

--- src/Package1/Package1Class.java
+++ src/Package1/Package1Class.java
@@ -3,6 +3,8 @@ package Package1;
 public class Package1Class {
 	public static void main(String[] args) {
 		System.out.println("Github practice 1");
+		
+	System.out.println("another print sttament");
 	}
 
 }
